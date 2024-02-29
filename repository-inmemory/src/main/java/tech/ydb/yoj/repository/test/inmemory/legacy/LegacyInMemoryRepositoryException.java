37f41bcf396169e0f7e2dbdb5475c5186b6f3da3
Alexander Lavrukov <lavrukov@yandex-team.ru>
2024-02-29 08:56:57 +0300

    remove-legacy-inmemory: Remove legacy inmemory repository

--- repository-inmemory/src/main/java/tech/ydb/yoj/repository/test/inmemory/legacy/LegacyInMemoryRepositoryException.java
+++
@@ -1,9 +0,0 @@
-package tech.ydb.yoj.repository.test.inmemory.legacy;
-
-import tech.ydb.yoj.repository.db.exception.RepositoryException;
-
-class LegacyInMemoryRepositoryException extends RepositoryException {
-    LegacyInMemoryRepositoryException(String message) {
-        super(message);
-    }
-}
