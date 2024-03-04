9363b1705788a88546f004ff49daa321f184008b
Nikolai Amelichev <entropia@nebius.com>
2024-03-04 17:49:08 +0100

    Make `@CustomValueType` annotation inheritable, so that ValueConverter logic can be reused for subclasses

--- databind/src/main/java/tech/ydb/yoj/databind/CustomValueType.java
+++ databind/src/main/java/tech/ydb/yoj/databind/CustomValueType.java
@@ -4,6 +4,7 @@ import tech.ydb.yoj.ExperimentalApi;
 import tech.ydb.yoj.databind.converter.ValueConverter;
 import tech.ydb.yoj.databind.schema.Schema;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -16,11 +17,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * Annotates the class, or entity field/record component as having a custom {@link ValueConverter value converter}.
  * <br>The specified converter will be used by YOJ instead of the default (Database column&harr;Java field) mapping.
  * <p>Annotation on entity field/record component has priority over annotation on the field's/record component's class.
+ * <p>This annotation is <em>inherited</em>, so make sure that your {@link #converter() converter} either supports all
+ * possible subclasses of your class, or restrict subclassing by making your class {@code final} or {@code sealed}.
  */
+@Inherited
 @Retention(RUNTIME)
 @Target({TYPE, FIELD, RECORD_COMPONENT})
 @ExperimentalApi(issue = "https://github.com/ydb-platform/yoj-project/issues/24")
-@SuppressWarnings("rawtypes")
 public @interface CustomValueType {
     /**
      * Simple value type that the {@link #converter()} represents a custom value type as.
@@ -32,10 +35,12 @@ public @interface CustomValueType {
      * Exact type of value that {@link #converter() converter's} {@link ValueConverter#toColumn(Schema.JavaField, Object) toColumn()} method returns.
      * Must implement {@link Comparable}.
      */
+    @SuppressWarnings("rawtypes")
     Class<? extends Comparable> columnClass();
 
     /**
      * Converter class. Must have a no-args public constructor.
      */
+    @SuppressWarnings("rawtypes")
     Class<? extends ValueConverter> converter();
 }
