10c049df099aa42f7c97815e593a6f8e352d6d66
g-sg-v <69846379+g-sg-v@users.noreply.github.com>
2024-03-25 23:16:19 +0100

    YOJ Tx Aspect (#53)
    Annotation and aspect to indirectly deal with yoj tx manager.
    
    Exmple of use:
    ```java
        @YojTransactional
        public void doSomethingInTx() {
        }
    ``` 
    annotation processor should be configured and aspect registed.

---
+++ aspect/src/main/java/tech/ydb/yoj/aspect/tx/YojTransactionAspect.java
@@ -0,0 +1,85 @@
+package tech.ydb.yoj.aspect.tx;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import tech.ydb.yoj.repository.db.Tx;
+import tech.ydb.yoj.repository.db.TxManager;
+import tech.ydb.yoj.repository.db.exception.RetryableException;
+
+/**
+ * The aim of the class is to eliminate direct <code>TxManager</code> usage
+ */
+@Aspect
+public class YojTransactionAspect {
+    private final TxManager tx;
+
+    public YojTransactionAspect(TxManager tx) {
+        this.tx = tx;
+    }
+
+    /**
+     * Annotation on the methos has priority over class
+     */
+    @Around("@within(transactional) && !@annotation(YojTransactional)")
+    public Object doInClassTransaction(ProceedingJoinPoint pjp, YojTransactional transactional) throws Throwable {
+        return doInTransaction(pjp, transactional);
+    }
+
+    @Around("@annotation(transactional)")
+    public Object doInMethodTransaction(ProceedingJoinPoint pjp, YojTransactional transactional) throws Throwable {
+        return doInTransaction(pjp, transactional);
+    }
+
+    private Object doInTransaction(ProceedingJoinPoint pjp, YojTransactional transactional) throws Throwable {
+        try {
+            String name = transactional.name().isBlank() ? getSimpleMethod(pjp.getSignature()) : transactional.name();
+            TxManager localTx = tx.withName(name);
+            if (Tx.Current.exists()) {
+                if (transactional.propagation() == YojTransactional.Propagation.NEVER) {
+                    throw new IllegalStateException("Transaction already exist!");
+                } else if (transactional.propagation() == YojTransactional.Propagation.REQUIRED) {
+                    return pjp.proceed();
+                } else if (transactional.propagation() == YojTransactional.Propagation.REQUIRES_NEW) {
+                    localTx = localTx.separate();
+                }
+            }
+            if (transactional.maxRetries() != YojTransactional.UNDEFINED) {
+                localTx = tx.withMaxRetries(transactional.maxRetries());
+            }
+            return transactional.readOnly() ? localTx.readOnly().run(() -> safeCall(pjp)) : localTx.tx(() -> safeCall(pjp));
+        } catch (CallRetryableException | CallException e) {
+            throw e.getCause();
+        }
+    }
+
+    private static String getSimpleMethod(Signature signature) {
+        return signature.getDeclaringType().getSimpleName() + "." + signature.getName();
+    }
+
+    Object safeCall(ProceedingJoinPoint pjp) {
+        try {
+            return pjp.proceed();
+        } catch (RetryableException e) {
+            throw new CallRetryableException(e);
+        } catch (Throwable e) {
+            throw new CallException(e);
+        }
+    }
+
+    /**
+     * It's a hint for tx manager to retry was requested
+     */
+    static class CallRetryableException extends RetryableException {
+        CallRetryableException(RetryableException e) {
+            super(e.getMessage(), e.getCause());
+        }
+    }
+
+    static class CallException extends RuntimeException {
+        CallException(Throwable e) {
+            super(e);
+        }
+    }
+}
---
+++ aspect/src/main/java/tech/ydb/yoj/aspect/tx/YojTransactional.java
@@ -0,0 +1,55 @@
+package tech.ydb.yoj.aspect.tx;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks method or class as code that should be executed in transaction
+ * <p>
+ * Real transaction is managed by aspect class
+ *
+ * @see YojTransactionAspect
+ */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface YojTransactional {
+    int UNDEFINED = -1;
+    /**
+     * Mark transactions as read only
+     */
+    boolean readOnly() default false;
+
+    /**
+     * Set transaction name otherwise method name will be used as transaction name
+     */
+    String name() default "";
+
+    /**
+     * Here I'm trying to force separate Service and Repository layers
+     */
+    Propagation propagation() default Propagation.NEVER;
+
+    /**
+     * Specifies custom retries count for method annotated
+     */
+    int maxRetries() default UNDEFINED;
+
+    enum Propagation {
+        /**
+         * Creates new transaction, throws exception if transaction already exists
+         */
+        NEVER,
+        /**
+         * Uses current transaction if exist or creates new otherwise
+         */
+        REQUIRED,
+        /**
+         * Always creates new transaction (Note, YDB doesn't support nested transactions)...
+         */
+        REQUIRES_NEW,
+    }
+}
