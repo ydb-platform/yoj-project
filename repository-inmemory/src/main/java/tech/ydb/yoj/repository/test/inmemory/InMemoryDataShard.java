14866245f261a10b6cb98ab6cbf6500cf134ab45
Sergei Galiamichev <g-sg-v@yandex.ru>
2024-03-26 13:53:50 +0100

    Replace TreeMap with EntityIdMap to reduce comparisons count

--- repository-inmemory/src/main/java/tech/ydb/yoj/repository/test/inmemory/InMemoryDataShard.java
+++ repository-inmemory/src/main/java/tech/ydb/yoj/repository/test/inmemory/InMemoryDataShard.java
@@ -18,18 +18,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 final class InMemoryDataShard<T extends Entity<T>> {
     private final TableDescriptor<T> tableDescriptor;
     private final EntitySchema<T> schema;
-    private final TreeMap<Entity.Id<T>, InMemoryEntityLine> entityLines;
+    private final Map<Entity.Id<T>, InMemoryEntityLine> entityLines;
     private final Map<Long, Set<Entity.Id<T>>> uncommited = new HashMap<>();
 
     private InMemoryDataShard(
             TableDescriptor<T> tableDescriptor,
             EntitySchema<T> schema,
-            TreeMap<Entity.Id<T>, InMemoryEntityLine> entityLines
+            Map<Entity.Id<T>, InMemoryEntityLine> entityLines
     ) {
         this.tableDescriptor = tableDescriptor;
         this.schema = schema;
@@ -44,12 +43,12 @@ final class InMemoryDataShard<T extends Entity<T>> {
         );
     }
 
-    private static <T extends Entity<T>> TreeMap<Entity.Id<T>, InMemoryEntityLine> createEmptyLines(Class<T> type) {
-        return new TreeMap<>(EntityIdSchema.getIdComparator(type));
+    private static <T extends Entity<T>> Map<Entity.Id<T>, InMemoryEntityLine> createEmptyLines(Class<T> type) {
+        return new EntityIdMap<>(EntityIdSchema.getIdComparator(type));
     }
 
     public synchronized InMemoryDataShard<T> createSnapshot() {
-        TreeMap<Entity.Id<T>, InMemoryEntityLine> snapshotLines = createEmptyLines(tableDescriptor.entityType());
+        Map<Entity.Id<T>, InMemoryEntityLine> snapshotLines = createEmptyLines(tableDescriptor.entityType());
         for (Map.Entry<Entity.Id<T>, InMemoryEntityLine> entry : entityLines.entrySet()) {
             snapshotLines.put(entry.getKey(), entry.getValue().createSnapshot());
         }
