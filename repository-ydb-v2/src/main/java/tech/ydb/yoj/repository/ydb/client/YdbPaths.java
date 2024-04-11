a7123cfab9dfab5f304b7909c21e5f0431a20a88
Nikolai Amelichev <entropia@nebius.com>
2024-04-11 17:13:26 +0200

    FIX: Disallow relative `tablespace` and `database` paths
    ...which were never supported but could be specified in `YdbConfig`

--- repository-ydb-v2/src/main/java/tech/ydb/yoj/repository/ydb/client/YdbPaths.java
+++ repository-ydb-v2/src/main/java/tech/ydb/yoj/repository/ydb/client/YdbPaths.java
@@ -1,23 +1,28 @@
 package tech.ydb.yoj.repository.ydb.client;
 
+import com.google.common.base.Preconditions;
+
 public final class YdbPaths {
     private YdbPaths() {
     }
 
     public static String canonicalTablespace(String tablespace) {
+        Preconditions.checkArgument(tablespace.startsWith("/"), "tablespace must be an absolute path, but got: '%s'", tablespace);
         return tablespace.endsWith("/") ? tablespace : tablespace + "/";
     }
 
     public static String canonicalRootDir(String tablespace) {
+        Preconditions.checkArgument(tablespace.startsWith("/"), "tablespace must be an absolute path, but got: '%s'", tablespace);
         return tablespace.endsWith("/") ? tablespace.substring(0, tablespace.length() - 1) : tablespace;
     }
 
     public static String canonicalDatabase(String database) {
-        return database == null ? null : database.endsWith("/") ? database.substring(0, database.length() - 1) : database;
+        Preconditions.checkArgument(database.startsWith("/"), "database path must be absolute, but got: '%s'", database);
+        return database.endsWith("/") ? database.substring(0, database.length() - 1) : database;
     }
 
     static String join(String parent, String child) {
-        return parent.isEmpty() ? child : canonicalTablespace(parent) + child;
+        return parent.isEmpty() ? child : (parent.endsWith("/") ? parent : parent + "/") + child;
     }
 
     public static String tableDirectory(String tablePath) {
