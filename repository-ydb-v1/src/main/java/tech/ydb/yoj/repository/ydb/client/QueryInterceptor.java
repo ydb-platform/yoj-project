9ef88c6213b46b0d2eabe88093ec27972a4f41b6
Nikolai Amelichev <1126790+nvamelichev@users.noreply.github.com>
2024-06-02 18:14:10 +0200

    Remove `yoj-repository-ydb-v1` module, support only YDB SDK v2.x (#74)

--- repository-ydb-v1/src/main/java/tech/ydb/yoj/repository/ydb/client/QueryInterceptor.java
+++
@@ -1,7 +0,0 @@
-package tech.ydb.yoj.repository.ydb.client;
-
-import com.yandex.ydb.table.Session;
-
-public interface QueryInterceptor {
-    void beforeExecute(QueryInterceptingSession.QueryType type, Session session, String query);
-}
