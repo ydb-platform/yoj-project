6963122b59d409871270b7369919f28b98f227da
Nikolai Amelichev <entropia@nebius.com>
2024-09-03 13:25:12 +0200

    Restore old-package `FieldValue` for maximum compatibility

--- databind/src/main/java/tech/ydb/yoj/databind/expression/values/FieldValue.java
+++ databind/src/main/java/tech/ydb/yoj/databind/expression/values/FieldValue.java
@@ -21,16 +21,19 @@ import java.util.UUID;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toCollection;
 
-public sealed interface FieldValue permits BooleanFieldValue, ByteArrayFieldValue, IntegerFieldValue,
+public sealed interface FieldValue extends tech.ydb.yoj.databind.expression.FieldValue
+        permits BooleanFieldValue, ByteArrayFieldValue, IntegerFieldValue,
         RealFieldValue, StringFieldValue, TimestampFieldValue, TupleFieldValue, UuidFieldValue {
 
     Optional<Comparable<?>> getComparableByType(Type fieldType, FieldValueType valueType);
 
+    @Override
     default Object getRaw(@NonNull JavaField field) {
         Comparable<?> cmp = getComparable(field);
         return CustomValueTypes.postconvert(field, cmp);
     }
 
+    @Override
     default Comparable<?> getComparable(@NonNull JavaField field) {
         field = field.isFlat() ? field.toFlatField() : field;
         FieldValueType valueType = FieldValueType.forSchemaField(field);
@@ -89,207 +92,5 @@ public sealed interface FieldValue permits BooleanFieldValue, ByteArrayFieldValu
         } else {
             return new Tuple(null, tupleValues(field.flatten().toList(), values));
         }
-    }
-
-    ///////////////////////////////////////////////
-    /// COMPATIBILITY QUERIES AND STATIC FACTORIES
-
-    /**
-     * @return {@code true} if this field value is an integer; {@code false} otherwise
-     * @see IntegerFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isNumber() {
-        return this instanceof IntegerFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is an floating-point number; {@code false} otherwise
-     * @see RealFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isReal() {
-        return this instanceof RealFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is a String; {@code false} otherwise
-     * @see StringFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isString() {
-        return this instanceof StringFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is an boolean; {@code false} otherwise
-     * @see BooleanFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isBool() {
-        return this instanceof BooleanFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is a timestamp; {@code false} otherwise
-     * @see TimestampFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isTimestamp() {
-        return this instanceof TimestampFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is a {@link Tuple tuple}; {@code false} otherwise
-     * @see TupleFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isTuple() {
-        return this instanceof TupleFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is a {@link ByteArray byte array}; {@code false} otherwise
-     * @see ByteArrayFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isByteArray() {
-        return this instanceof ByteArrayFieldValue;
-    }
-
-    /**
-     * @return {@code true} if this field value is an {@link UUID}; {@code false} otherwise
-     * @see UuidFieldValue
-     * @deprecated We recommend using
-     * <a href="https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html">Pattern Matching for {@code instanceof}
-     * </a> (Java 17+) or <a href="https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html">Pattern
-     * Matching for {@code switch} Expressions and Statements</a> (Java 21+), because {@code FieldValue} is a {@code sealed} interface.
-     */
-    @Deprecated
-    default boolean isUuid() {
-        return this instanceof UuidFieldValue;
-    }
-
-    /**
-     * Constructs a new field value that is definitely a String. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit conversions from
-     * a enum or a custom value type.)
-     *
-     * @param str string value
-     * @return field value that holds a specified string value
-     */
-    @NonNull
-    static FieldValue ofStr(@NonNull String str) {
-        return new StringFieldValue(str);
-    }
-
-    /**
-     * Constructs a new field value that is definitely an integer. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit conversions
-     * from a custom value type.)
-     *
-     * @param num integer value
-     * @return field value that holds the specified integer value
-     */
-    @NonNull
-    static FieldValue ofNum(long num) {
-        return new IntegerFieldValue(num);
-    }
-
-    /**
-     * Constructs a new field value that is definitely a floating-point value. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit
-     * conversions from a custom value type.)
-     *
-     * @param real floating-point value
-     * @return field value that holds the specified floating-point value
-     */
-    @NonNull
-    static FieldValue ofReal(double real) {
-        return new RealFieldValue(real);
-    }
-
-    /**
-     * Constructs a new field value that is definitely a boolean value. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit
-     * conversions from a custom value type.)
-     *
-     * @param bool boolean value
-     * @return field value that holds the specified boolean value
-     */
-    @NonNull
-    static FieldValue ofBool(boolean bool) {
-        return new BooleanFieldValue(bool);
-    }
-
-    /**
-     * Constructs a new field value that is definitely a timestamp value. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit
-     * conversions from a custom value type.)
-     *
-     * @param timestamp timestamp value
-     * @return field value that holds the specified timestamp value
-     */
-    @NonNull
-    static FieldValue ofTimestamp(@NonNull Instant timestamp) {
-        return new TimestampFieldValue(timestamp);
-    }
-
-    /**
-     * Constructs a new field value that is a tuple.
-     *
-     * @param tuple tuple value
-     * @return field value that holds the specified timestamp value
-     */
-    @NonNull
-    static FieldValue ofTuple(@NonNull Tuple tuple) {
-        return new TupleFieldValue(tuple);
-    }
-
-    /**
-     * Constructs a new field value that is definitely a byte array value. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit
-     * conversions from a custom value type.)
-     *
-     * @param byteArray byte array value
-     * @return field value that holds the specified byte array value
-     */
-    @NonNull
-    static FieldValue ofByteArray(@NonNull ByteArray byteArray) {
-        return new ByteArrayFieldValue(byteArray);
-    }
-
-    /**
-     * Constructs a new field value that is definitely an UUID value. (Unlike {@link #ofObj(Object, JavaField)} which may perform implicit
-     * conversions from a custom value type.)
-     *
-     * @param uuid UUID value
-     * @return field value that holds the specified UUID value
-     */
-    @NonNull
-    static FieldValue ofUuid(@NonNull UUID uuid) {
-        return new UuidFieldValue(uuid);
     }
 }
