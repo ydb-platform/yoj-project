c4c51c771e068eba41c300601d2a08afdbee7619
Sergei Galiamichev <69846379+g-sg-v@users.noreply.github.com>
2024-12-27 16:05:28 +0100

    Query multiple tables using one entity (#94)
    Co-authored-by: Sergei Galiamichev <gsv@nebius.com>

--- databind/src/main/java/tech/ydb/yoj/ExperimentalApi.java
+++ databind/src/main/java/tech/ydb/yoj/ExperimentalApi.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
@@ -14,7 +15,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  * Annotates <em>experimental features</em>. These features are not part of the stable YOJ API: they can change incompatibly,
  * or even disappear entirely <em>in any release</em>.
  */
-@Target({TYPE, FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, ANNOTATION_TYPE})
 @Retention(SOURCE)
 public @interface ExperimentalApi {
     /**
