e910cef946d19967b3ec9b49060e0f11b14feca3
Andrey Alekseev <Alekseev.Andrey.V@yandex.ru>
2025-01-15 12:33:17 +0300

    #117: More consistent column naming for subobject fields
    Instead of the legacy YOJ 1.x and 2.x strategy that adds subobject column name as prefix for subobjects' column names
    **unless an explicit `Column#name()` annotation is set**, introduce two more consistent strategies:
    - Always add subobject column name as prefix (`ColumnNaming.RELATIVE`)
    - Never add subobject column name as prefix, treat subobject column as top-level (`ColumnNaming.ABSOLUTE`)
    with the more generally applicable `ColumnNaming.RELATIVE` strategy becoming the default in YOJ 3.0.

--- databind/src/main/java/tech/ydb/yoj/databind/schema/naming/AnnotationFirstNamingStrategy.java
+++ databind/src/main/java/tech/ydb/yoj/databind/schema/naming/AnnotationFirstNamingStrategy.java
@@ -1,6 +1,7 @@
 package tech.ydb.yoj.databind.schema.naming;
 
 import lombok.NonNull;
+import tech.ydb.yoj.databind.schema.Column;
 import tech.ydb.yoj.databind.schema.Schema;
 import tech.ydb.yoj.databind.schema.Table;
 
@@ -30,17 +31,34 @@ public class AnnotationFirstNamingStrategy implements NamingStrategy {
 
     private String getColumnName(Schema.JavaField field) {
         var annotation = field.getField().getColumn();
-        if (annotation != null && !annotation.name().isEmpty()) {
-            return annotation.name();
+        var parentName = (field.getParent() == null) ? null : field.getParent().getName();
+
+        if (annotation == null) {
+            var fieldName = field.getField().getName();
+            return parentName == null ? fieldName : parentName + NAME_DELIMITER + fieldName;
         }
 
-        return getColumnNameFromField(field);
+        return getColumnNameWithNaming(field, annotation.name(), parentName, annotation.columnNaming());
     }
 
-    protected String getColumnNameFromField(Schema.JavaField field) {
-        return (field.getParent() == null)
-                ? field.getField().getName()
-                : field.getParent().getName() + NAME_DELIMITER + field.getField().getName();
+    private static String getColumnNameWithNaming(
+        Schema.JavaField field,
+        String annotationName,
+        String parentName, Column.ColumnNaming columnNaming
+    ) {
+        var name = (annotationName.isEmpty()) ? field.getField().getName() : annotationName;
+
+        return switch (columnNaming) {
+            case ABSOLUTE -> name;
+            case RELATIVE -> parentName + NAME_DELIMITER + name;
+            case LEGACY -> {
+                if (!annotationName.isEmpty()) {
+                    yield annotationName;
+                } else {
+                    yield parentName == null ? name : parentName + NAME_DELIMITER + name;
+                }
+            }
+        };
     }
 
     protected void propagateFieldNameToFlatSubfield(Schema.JavaField field) {
