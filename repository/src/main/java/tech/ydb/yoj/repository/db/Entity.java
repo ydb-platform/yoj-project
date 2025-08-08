4bf46274a262dd745a57de3a3669b5636fd079d1
Nikolai Amelichev <entropia@nebius.com>
2025-08-08 20:54:53 +0200

    noticket: Deprecate `Entity.Id.{getType(), isPartial()}` for removal, and use Guava `TypeToken` only where it's absolutely needed

--- repository/src/main/java/tech/ydb/yoj/repository/db/Entity.java
+++ repository/src/main/java/tech/ydb/yoj/repository/db/Entity.java
@@ -60,17 +60,31 @@ public interface Entity<E extends Entity<E>> extends Table.ViewId<E> {
             return Tx.Current.get().getRepositoryTransaction().table(getType()).find(this, throwIfAbsent);
         }
 
+        /**
+         * @deprecated This method will be removed in YOJ 3.0.0. Please stop using it.
+         */
         @SuppressWarnings("unchecked")
+        @Deprecated(forRemoval = true)
         default Class<E> getType() {
+            DeprecationWarnings.warnOnce(
+                    "Entity.Id.getType()",
+                    "You are using Entity.Id.getType() which will be removed in YOJ 3.0.0. Please stop using this method"
+            );
             return (Class<E>) new TypeToken<E>(getClass()) {
             }.getRawType();
         }
 
+        /**
+         * @deprecated This method will be removed in YOJ 3.0.0. Please use other ways to check if ID is partial
+         * (i.e., has some of its trailing components set to {@code null} to implicitly indicate an <em>ID range</em>.)
+         */
+        @Deprecated(forRemoval = true)
         default boolean isPartial() {
-            var schema = EntitySchema.of(getType()).getIdSchema();
-            var columns = schema.flattenFields();
-            var nonNullFields = schema.flatten(this);
-            return columns.size() > nonNullFields.size();
+            DeprecationWarnings.warnOnce(
+                    "Entity.Id.isPartial()",
+                    "You are using Entity.Id.isPartial() which will be removed in YOJ 3.0.0. Please use other ways to check if ID represents a range"
+            );
+            return TableQueryImpl.isPartialId(this, EntitySchema.of(getType()));
         }
     }
 }
