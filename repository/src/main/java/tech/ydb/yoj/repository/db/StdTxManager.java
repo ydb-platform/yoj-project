89eea855a2c52a851f40ffc79c0c4a179b62c964
Nikolai Amelichev <entropia@nebius.com>
2026-01-27 16:17:54 +0100

    #209: FIX: Observe transaction attempts metric only on transaction result (both failure and success), not on each transaction attempt start

--- repository/src/main/java/tech/ydb/yoj/repository/db/StdTxManager.java
+++ repository/src/main/java/tech/ydb/yoj/repository/db/StdTxManager.java
@@ -16,15 +16,16 @@ import tech.ydb.yoj.ExperimentalApi;
 import tech.ydb.yoj.repository.db.cache.TransactionLog;
 import tech.ydb.yoj.repository.db.exception.QueryInterruptedException;
 import tech.ydb.yoj.repository.db.exception.RetryableException;
+import tech.ydb.yoj.util.lang.Interrupts;
 import tech.ydb.yoj.util.lang.Strings;
 import tech.ydb.yoj.util.log.MdcSetup;
 
 import javax.annotation.Nullable;
 import java.time.Duration;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 
-import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static tech.ydb.yoj.repository.db.IsolationLevel.ONLINE_CONSISTENT_READ_ONLY;
 import static tech.ydb.yoj.repository.db.IsolationLevel.SERIALIZABLE_READ_WRITE;
@@ -65,8 +66,16 @@ public final class StdTxManager implements TxManager, TxManagerState {
             .labelNames("tx_name")
             .buckets(DURATION_BUCKETS)
             .register();
-    private static final Histogram attempts = Histogram.build("tx_attempts", "Tx attempts spent to success")
-            .labelNames("tx_name")
+    private static final Histogram attempts = Histogram.build("tx_attempts", "Total tx attempts to completion")
+            // tx_name: <transaction name>
+            // result_type:
+            //     success
+            //   | interrupt
+            //   | failure_nonretryable
+            //   | failure_max_retries
+            //   | failure_deferred_finally
+            // reason: exception type name, if any, e.g. "QueryInterrupted"
+            .labelNames("tx_name", "result_type", "reason")
             .buckets(TX_ATTEMPTS_BUCKETS)
             .register();
     private static final Counter results = Counter.build("tx_result", "Tx commits/rollbacks/fails")
@@ -189,60 +198,10 @@ public final class StdTxManager implements TxManager, TxManagerState {
     @Override
     public <T> T tx(Supplier<T> supplier) {
         TxName txName = txNameGenerator.generate();
-        String name = txName.name();
-
         checkSeparatePolicy(separatePolicy, txName.logName());
 
-        long txLogId = txLogIdSeq.incrementAndGet();
-
-        RetryableException lastRetryableException = null;
-        TxImpl lastTx = null;
-        Timer totalTimer = totalDuration.labels(name).startTimer();
-        MdcSetup mdcs = txMdcs(txName, txLogId);
-        try {
-            for (int attempt = 1; attempt <= maxAttemptCount; attempt++) {
-                attempts.labels(name).observe(attempt);
-                mdcs.put("tx-attempt", attempt);
-                try {
-                    T result;
-                    try (Timer ignored = attemptDuration.labels(name).startTimer()) {
-                        RepositoryTransaction transaction = repository.startTransaction(options);
-                        lastTx = new TxImpl(name, transaction, options);
-                        result = lastTx.run(supplier);
-                    }
-
-                    if (options.isDryRun()) {
-                        results.labels(name, "rollback").inc();
-                        results.labels(name, "dry_run").inc();
-                    } else {
-                        results.labels(name, "commit").inc();
-                    }
-                    return result;
-                } catch (RetryableException e) {
-                    retries.labels(name, getExceptionNameForMetric(e)).inc();
-                    lastRetryableException = e;
-                    if (attempt + 1 <= maxAttemptCount) {
-                        sleepBeforeNextAttempt(e, attempt);
-                    }
-                } catch (Exception e) {
-                    results.labels(name, "rollback").inc();
-                    throw e;
-                }
-            }
-            results.labels(name, "fail").inc();
-
-            throw requireNonNull(lastRetryableException).rethrow();
-        } finally {
-            // If we use try-with-resources with `totalTimer`, the total tx duration won't include the duration
-            // of lastTx.runDeferredFinally()! So we manually stop the timer *after* we attempt to run deferred logic:
-            try {
-                if (!options.isDryRun() && lastTx != null) {
-                    lastTx.runDeferredFinally();
-                }
-            } finally {
-                totalTimer.close();
-            }
-            mdcs.restore();
+        try (RetryableTx<T> retryableTx = new RetryableTx<>(txName)) {
+            return retryableTx.run(supplier);
         }
     }
 
@@ -261,38 +220,6 @@ public final class StdTxManager implements TxManager, TxManagerState {
             case LOG -> log.warn("""
                     Transaction '{}' was run when another transaction is active. Perhaps unexpected behavior. \
                     Use TxManager.separate() to avoid this message""", txName);
-        }
-    }
-
-    private String getExceptionNameForMetric(RetryableException e) {
-        return Strings.removeSuffix(e.getClass().getSimpleName(), "Exception");
-    }
-
-    private MdcSetup txMdcs(TxName txName, long txLogId) {
-        MdcSetup mdcStack = new MdcSetup();
-        mdcStack.put("tx", formatTx(txName, txLogId))
-                .put("tx-id", formatTxId(txLogId))
-                .put("tx-name", txName.logName());
-        if (logContext != null) {
-            mdcStack.put("tx-context", logContext);
-        }
-        return mdcStack;
-    }
-
-    private String formatTx(TxName txName, long txLogId) {
-        return formatTxId(txLogId) + " {" + txName.logName() + (logContext != null ? "/" + logContext : "") + "}";
-    }
-
-    private String formatTxId(long txLogId) {
-        return Strings.leftPad(Long.toUnsignedString(txLogId, 36), 6, '0') + options.getIsolationLevel().getTxIdSuffix();
-    }
-
-    private static void sleepBeforeNextAttempt(RetryableException e, int attempt) {
-        try {
-            MILLISECONDS.sleep(e.getRetryPolicy().calcDuration(attempt).toMillis());
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-            throw new QueryInterruptedException("DB query interrupted", ex);
         }
     }
 
@@ -386,6 +313,175 @@ public final class StdTxManager implements TxManager, TxManagerState {
         @Override
         public <T> T run(Supplier<T> supplier) throws RetryableException {
             return StdTxManager.this.withOptions(options).tx(supplier);
+        }
+    }
+
+    private class RetryableTx<T> implements AutoCloseable {
+        private final String name;
+        private final Timer totalTimer;
+        private final MdcSetup mdcs;
+
+        private int attempt = 0;
+        private TxImpl tx = null;
+
+        private Exception exception = null;
+        private String resultType = null;
+        private String errorType = "";
+
+        private RetryableTx(TxName txName) {
+            this.name = txName.name();
+            this.totalTimer = totalDuration.labels(name).startTimer();
+            this.mdcs = txMdcs(txName, txLogIdSeq.incrementAndGet());
+        }
+
+        private MdcSetup txMdcs(TxName txName, long txLogId) {
+            MdcSetup mdcStack = new MdcSetup();
+            mdcStack.put("tx", formatTx(txName, txLogId))
+                    .put("tx-id", formatTxId(txLogId))
+                    .put("tx-name", txName.logName());
+            if (logContext != null) {
+                mdcStack.put("tx-context", logContext);
+            }
+            return mdcStack;
+        }
+
+        private String formatTx(TxName txName, long txLogId) {
+            return formatTxId(txLogId) + " {" + txName.logName() + (logContext != null ? "/" + logContext : "") + "}";
+        }
+
+        private String formatTxId(long txLogId) {
+            return Strings.leftPad(Long.toUnsignedString(txLogId, 36), 6, '0')
+                    + options.getIsolationLevel().getTxIdSuffix();
+        }
+
+        public T run(Supplier<T> supplier) {
+            Preconditions.checkArgument(attempt == 0, "Can only call RetryableTx.run() once");
+            Objects.requireNonNull(supplier, "supplier must not be null");
+
+            try {
+                return runWithRetries(supplier);
+            } catch (Exception e) {
+                exception = e;
+                throw e;
+            }
+        }
+
+        private T runWithRetries(Supplier<T> supplier) {
+            while (true) {
+                attempt++;
+                mdcs.put("tx-attempt", attempt);
+                try (Timer ignored = attemptDuration.labels(name).startTimer()) {
+                    return runAttempt(supplier);
+                } catch (RetryableException e) {
+                    onRetryableException(e);
+                    if (attempt < maxAttemptCount) {
+                        sleepBeforeNextAttempt(e);
+                    } else {
+                        onRetriesFailed(e);
+                        throw e.rethrow();
+                    }
+                } catch (Exception e) {
+                    onNonretryableException(e);
+                    throw e;
+                }
+            }
+        }
+
+        private T runAttempt(Supplier<T> supplier) {
+            tx = null;
+
+            RepositoryTransaction transaction = repository.startTransaction(options);
+            tx = new TxImpl(name, transaction, options);
+
+            T result = tx.run(supplier);
+            onAttemptSuccess();
+            return result;
+        }
+
+        private String getExceptionNameForMetric(Exception e) {
+            return Strings.removeSuffix(e.getClass().getSimpleName(), "Exception");
+        }
+
+        private void sleepBeforeNextAttempt(RetryableException e) {
+            try {
+                MILLISECONDS.sleep(e.getRetryPolicy().calcDuration(attempt).toMillis());
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+
+                var qie = new QueryInterruptedException("DB query interrupted", ie);
+                onNonretryableException(qie);
+                throw qie;
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                if (!options.isDryRun() && tx != null) {
+                    tx.runDeferredFinally();
+                }
+            } catch (Exception e) {
+                if (exception == null) {
+                    // Transaction has committed successfully, but its deferredFinally block failed
+                    // Throw the deferred block's exception:
+                    onDeferredFinallyException(e);
+                    throw e;
+                } else {
+                    // An exception was already thrown by the transaction body, and then its deferedFinally block failed
+                    // To keep valuable stack trace info, add the deferredFinally exception as suppressed:
+                    exception.addSuppressed(e);
+                }
+            } finally {
+                totalTimer.observeDuration();
+                mdcs.restore();
+                tx = null;
+
+                onClose();
+            }
+        }
+
+        private void onAttemptSuccess() {
+            if (options.isDryRun()) {
+                results.labels(name, "rollback").inc();
+                results.labels(name, "dry_run").inc();
+            } else {
+                results.labels(name, "commit").inc();
+            }
+
+            resultType = "success";
+            errorType = "";
+        }
+
+        private void onRetryableException(RetryableException e) {
+            retries.labels(name, getExceptionNameForMetric(e)).inc();
+        }
+
+        private void onRetriesFailed(RetryableException e) {
+            results.labels(name, "fail").inc();
+
+            resultType = "failure_max_retries";
+            errorType = getExceptionNameForMetric(e);
+        }
+
+        private void onNonretryableException(Exception e) {
+            if (Interrupts.isInterruptException(e)) {
+                results.labels(name, "interrupt").inc();
+                resultType = "interrupt";
+            } else {
+                results.labels(name, "rollback").inc();
+                resultType = "failure_nonretryable";
+            }
+            errorType = getExceptionNameForMetric(e);
+        }
+
+        private void onDeferredFinallyException(Exception e) {
+            resultType = "failure_deferred_finally";
+            errorType = getExceptionNameForMetric(e);
+        }
+
+        private void onClose() {
+            attempts.labels(name, resultType, errorType)
+                    .observe(attempt);
         }
     }
 
