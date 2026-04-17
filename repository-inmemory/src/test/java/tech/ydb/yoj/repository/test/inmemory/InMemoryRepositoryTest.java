c7d1130febe208d0a21189314778739644935f01
Anton Tolstov <anaym@users.noreply.github.com>
2026-04-17 06:47:07 +0200

    Test bulkUpdate InMemoryRepositoryTest.java

--- repository-inmemory/src/test/java/tech/ydb/yoj/repository/test/inmemory/InMemoryRepositoryTest.java
+++ repository-inmemory/src/test/java/tech/ydb/yoj/repository/test/inmemory/InMemoryRepositoryTest.java
@@ -1,11 +1,42 @@
 package tech.ydb.yoj.repository.test.inmemory;
 
+import org.junit.Test;
 import tech.ydb.yoj.repository.db.Repository;
+import tech.ydb.yoj.repository.db.bulk.BulkParams;
 import tech.ydb.yoj.repository.test.RepositoryTest;
+import tech.ydb.yoj.repository.test.sample.model.Project;
 
-public class InMemoryRepositoryTest extends RepositoryTest {
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InMemoryRepositoryTest extends RepositoryTest<TestInMemoryRepository> {
+
     @Override
-    protected Repository createTestRepository() {
+    protected Repository createDb() {
         return new TestInMemoryRepository();
+    }
+
+    @Test
+    public void bulkUpsert() {
+        Project.Id existingId = new Project.Id("existing");
+        Project.Id newId = new Project.Id("new");
+
+        db.tx(() -> db.projects().save(new Project(existingId, "before")));
+
+        db.tx(() -> db.projects().bulkUpsert(
+                List.of(
+                        new Project(existingId, "after"),
+                        new Project(newId, "created")
+                ),
+                BulkParams.DEFAULT
+        ));
+
+        List<Project> projects = db.tx(() -> db.projects().findAll());
+
+        assertThat(projects).containsExactlyInAnyOrder(
+                new Project(existingId, "after"),
+                new Project(newId, "created")
+        );
     }
 }
